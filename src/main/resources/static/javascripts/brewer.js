1fa673e1e76f9d82c14b5ddaf91650f8521607de
glauberjsilva <gjsilva83@gmail.com>
2018-08-31 16:47:24 -0300

    Modularizando o javascript brewer.js

--- src/main/resources/static/javascripts/brewer.js
+++ src/main/resources/static/javascripts/brewer.js
@@ -1,7 +1,31 @@
+var Brewer = Brewer || {};
+
+Brewer.MaskMoney = (function() {
+	
+	function MaskMoney() {
+		this.decimal = $('.js-decimal');
+		this.plain = $('.js-plain');
+	}
+	
+	MaskMoney.prototype.enable = function() {
+		this.decimal.maskMoney({ decimal: ',', thousands: '.' });
+		this.plain.maskMoney({ precision : 0, thousands: '.', decimal:''});
+	}
+	
+	return MaskMoney;
+
+}());
+
 $(function() {
+	var maskMoney = new Brewer.MaskMoney();
+	maskMoney.enable();
+)};
+
+/*$(function() {
 	var decimal = $('.js-decimal');
 	decimal.maskMoney({ decimal: ',', thousands: '.' });
 
 	var plain = $('.js-plain');
 	plain.maskMoney({ precision : 0, thousands: '.' });
 })
+*/
